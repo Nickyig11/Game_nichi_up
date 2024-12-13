e4d34de9fcf19d306d1294b86c76fea466434cb2
Nickyig11 <Nickyig11@gmail.com>
2024-12-13 17:10:48 +0100

    añadido salto

--- nichiUp_proyectoUnity/Assets/Scripts/PlayerMove.cs
+++ nichiUp_proyectoUnity/Assets/Scripts/PlayerMove.cs
@@ -10,20 +10,42 @@ public class PlayerMove : MonoBehaviour
 
     public Animator animator;
 
+    public Rigidbody rb;
+    public float fuerzaSalto = 8;
+    public bool puedoSaltar;
+
     private float x, y;
 
+    void FixedUpdate()
+    {
+        transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * runSpeed);
+    }
     void Update()
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
 
-        transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
-
-        transform.Translate(0, 0, y * Time.deltaTime * runSpeed);
-
         
         animator.SetFloat("VelX", x);
         animator.SetFloat("VelY", y);
+
+        if(puedoSaltar)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetBool("salte", true);
+                rb.AddForce(new Vector3(0,fuerzaSalto,0),ForceMode.Impulse);
+            }
+            animator.SetBool("tocoSuelo",true);
+        }else
+        {
+            EstoyCayendo();
+        }
+    }
+    public void EstoyCayendo(){
+        animator.SetBool("tocoSuelo",false);
+        animator.SetBool("salte", false);
     }
 
 }
